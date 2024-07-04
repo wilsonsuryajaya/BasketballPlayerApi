969a0f15b3e6e15a7213859c567d797e067a71fa
Wilson S Leoputra <wilsonsuryajaya@gmail.com>
2024-07-04 11:19:00 +0800

    Update EF

--- BasketballPlayerApi/Models/Player.cs
+++ BasketballPlayerApi/Models/Player.cs
@@ -1,15 +1,16 @@
 ﻿namespace BasketballPlayerApi.Models
 {
     /// <summary>
+    /// dotnet tool install --global dotnet-ef --version 6.*
     /// dotnet ef dbcontext scaffold "Server=.;Database=SampleDatabase;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models --force
+    /// dotnet ef dbcontext scaffold "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Databases\\SampleDB.mdf;Integrated Security=True;Connect Timeout=30" Microsoft.EntityFrameworkCore.SqlServer -o Models --force
     /// </summary>
     public partial class Player
     {
         public int PlayerId { get; set; }
-        public string? Name { get; set; }
-        public int? JerseyNumber { get; set; }
-        public int? TeamId { get; set; }
 
-        //public virtual Team? Team { get; set; }
+        public string? Name { get; set; }
+
+        public int? JerseyNumber { get; set; }
     }
 }
--- BasketballPlayerApi/Models/SampleDatabaseContext.cs
+++ BasketballPlayerApi/Models/SampleDatabaseContext.cs
@@ -24,7 +24,7 @@ namespace BasketballPlayerApi.Models
             if ( !optionsBuilder.IsConfigured )
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer( "Server=.;Database=SampleDatabase;Trusted_Connection=True;" );
+                optionsBuilder.UseSqlServer( "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Databases\\SampleDB.mdf;Integrated Security=True;Connect Timeout=30" );
             }
         }
 
@@ -41,26 +41,8 @@ namespace BasketballPlayerApi.Models
                 entity.Property( e => e.Name )
                     .HasMaxLength( 50 )
                     .IsUnicode( false )
-                    .HasColumnName( "playerName" );
-
-                entity.Property( e => e.TeamId ).HasColumnName( "teamId" );
-
-                //entity.HasOne( d => d.Team )
-                //    .WithMany( p => p.Players )
-                //    .HasForeignKey( d => d.TeamId )
-                //    .HasConstraintName( "FK_Player_Team" );
+                    .HasColumnName( "name" );
             } );
-
-            //modelBuilder.Entity<Team>( entity =>
-            //{
-            //    entity.ToTable( "Team" );
-
-            //    entity.Property( e => e.TeamId ).HasColumnName( "teamId" );
-
-            //    entity.Property( e => e.TeamName )
-            //        .HasMaxLength( 50 )
-            //        .HasColumnName( "teamName" );
-            //} );
 
             OnModelCreatingPartial( modelBuilder );
         }
