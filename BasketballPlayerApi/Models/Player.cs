968b48ad076f59eb8581a6a8d80cac73aed34ed5
Wilson S Leoputra <wilsonsuryajaya@gmail.com>
2024-07-15 13:50:43 +0800

    Add comment

--- BasketballPlayerApi/Models/Player.cs
+++ BasketballPlayerApi/Models/Player.cs
@@ -1,6 +1,7 @@
 ﻿namespace BasketballPlayerApi.Models
 {
     /// <summary>
+    /// Entity Framework
     /// dotnet tool install --global dotnet-ef --version 6.*
     /// dotnet ef dbcontext scaffold "Server=.;Database=SampleDatabase;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models --force
     /// dotnet ef dbcontext scaffold "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Databases\\SampleDB.mdf;Integrated Security=True;Connect Timeout=30" Microsoft.EntityFrameworkCore.SqlServer -o Models --force
